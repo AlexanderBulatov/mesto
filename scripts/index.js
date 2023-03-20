704470660df228b306852d9845171d49925c3287
Alexander Bulatov <bulatovdrive@yandex.ru>
2023-03-20 17:25:02 +0300

    fix: исправить замечание ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
-let popupInputName = document.querySelector('.popup-edit-profile__input_type_name');
-let popupInputOccupation = document.querySelector('.popup-edit-profile__input_type_occupation');
-let profileName = document.querySelector('.profile__name');
-let profileOccupation = document.querySelector('.profile__occupation');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputOccupation = document.querySelector('.popup__input_type_occupation');
+const profileName = document.querySelector('.profile__name');
+const profileOccupation = document.querySelector('.profile__occupation');
+const popup = document.querySelector('.popup');
+const pofileEditBttn = document.querySelector('.profile__edit');
+const popupSubmitBttn = document.querySelector('.popup__form');
+const popupCloseBttn = document.querySelector('.popup__close');
+
 // open popup window
 function openPopupProfile (){
-  document.querySelector('.body__popup').classList.add('popup_open');
-  document.querySelector('.body__content').classList.add('page_scroll-off');
+  popup.classList.add('popup_opened');
   popupInputName.value = profileName.textContent;
   popupInputOccupation.value = profileOccupation.textContent;
 }
+
 // close popup window
 function closePopupProfile (){
-  document.querySelector('.body__popup').classList.remove('popup_open');
-  document.querySelector('.body__content').classList.remove('page_scroll-off');
+  popup.classList.remove('popup_opened');
 }
-// submit and close popup window if inputs isn't equal ''
+
+// submit and close popup window
 function handleFormSubmit (e) {
     e.preventDefault();
-
-    if(popupInputName.value !=='' && popupInputOccupation.value !==''){
-      profileName.textContent = popupInputName.value;
-      profileOccupation.textContent = popupInputOccupation.value;
-      closePopupProfile();
-    }
+    profileName.textContent = popupInputName.value;
+    profileOccupation.textContent = popupInputOccupation.value;
+    closePopupProfile();
 }
+
 // add listeners to editing profile and popup btn-s
-document.querySelector('.profile__edit').addEventListener('click',openPopupProfile);
-document.querySelector('.popup__close').addEventListener('click',closePopupProfile);
-document.querySelector('.popup__form').addEventListener('submit', handleFormSubmit);
+pofileEditBttn.addEventListener('click',openPopupProfile);
+popupCloseBttn.addEventListener('click',closePopupProfile);
+popupSubmitBttn.addEventListener('submit', handleFormSubmit);
