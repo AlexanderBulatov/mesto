ad1fd35846ddca0955d960308c2844cfcb7c176b
Alexander Bulatov <bulatovdrive@yandex.ru>
2023-04-02 13:00:58 +0300

    feat: добавить alt для изображений

--- scripts/index.js
+++ scripts/index.js
@@ -1,116 +1,90 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]
+import { initialCards } from './initial.js';
 
-//profile popup fields
+//---------------------- profile popup fields
 const popupInputName = document.querySelector('.popup__input_type_name');
 const popupInputOccupation = document.querySelector('.popup__input_type_occupation');
+//---------------------- add place popup fields
 const popupInputCaption = document.querySelector('.popup__input_type_caption');
 const popupInputLink = document.querySelector('.popup__input_type_link');
-
+//---------------------- picture popup elements
 const popupPicture = document.querySelector('.popup__picture');
 const popupPictureCaption = document.querySelector('.popup__picture-caption');
 
-// //profile titles
+//---------------------- profile titles
 const profileName = document.querySelector('.profile__name');
 const profileOccupation = document.querySelector('.profile__occupation');
 
-// // popups windows
+//---------------------- popups windows
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupAddPlace = document.querySelector('.popup_type_add-picture');
-const popupZoomFoto = document.querySelector('.popup_type_picture');
+const popupZoomPicture = document.querySelector('.popup_type_picture');
 
+//---------------------- popups forms
 const profileForm = popupProfile.querySelector('.popup__form');
 const addPlaceForm = popupAddPlace.querySelector('.popup__form');
 
-const elementFoto = document.querySelector('.element__foto');
-
-// // interface buttons
+//---------------------- interface buttons
 const profileEditBttn = document.querySelector('.profile__edit');
 const placeAddBttn = document.querySelector('.profile__add-place');
 const popupCloseBttns = document.querySelectorAll('.popup__close');
 
-
-// point for adding place Items in DOM
+//---------------------- point for adding place Items in DOM
 const places = document.querySelector('.elements');
-
+//----------------------
 const placeTemplate = document.querySelector('.template-element').content;
 
-// preparing Place Item
+//======================== preparing Place Item - START
 const createPlaceItem = (placeData) => {
-  const placeItem = placeTemplate.querySelector('.element').cloneNode(true);
-  const placeCaption = placeItem.querySelector('.element__name');
-  const placePicture = placeItem.querySelector('.element__foto');
+    const placeItem = placeTemplate.querySelector('.element').cloneNode(true);
+    const placeCaption = placeItem.querySelector('.element__name');
+    const placePicture = placeItem.querySelector('.element__foto');
 
-  placeCaption.textContent = placeData.name;
-  placePicture.src = placeData.link;
-  placePicture.alt = placeData.name;
+    placeCaption.textContent = placeData.name;
+    placePicture.src = placeData.link;
+    placePicture.alt = placeData.name;
 
-  const likeBttn = placeItem.querySelector('.element__like');
-  const deleteBttn = placeItem.querySelector('.element__delete');
-  const zoomPicture = placeItem.querySelector('.element__foto');
+    const likeBttn = placeItem.querySelector('.element__like');
+    const deleteBttn = placeItem.querySelector('.element__delete');
+    const zoomPicture = placeItem.querySelector('.element__foto');
 
-  const handleLike = () => {
-    likeBttn.classList.toggle('element__like_active');
-  };
-  const handleDelete = () => {
-    placeItem.remove();
-  };
-//===============================
+    const handleLike = () => {
+      likeBttn.classList.toggle('element__like_active');
+    };
 
-  const handleZoom = () => {
-    popupPicture.src = placePicture.src;
-    popupPictureCaption.textContent = placeCaption.textContent;
-    openPopup (popupZoomFoto);
-  };
-//=================================
-  likeBttn.addEventListener('click', handleLike);
-  deleteBttn.addEventListener('click', handleDelete);
-  zoomPicture.addEventListener('click', handleZoom);
+    const handleDelete = () => {
+      placeItem.remove();
+    };
 
-  return placeItem;
-}
+    const handleZoom = () => {
+      popupPicture.src = placePicture.src;
+      popupPictureCaption.textContent = placeCaption.textContent;
+      popupPicture.alt = placeData.name;
+      openPopup (popupZoomPicture);
+    };
 
-// show Place Item with data
+    likeBttn.addEventListener('click', handleLike);
+    deleteBttn.addEventListener('click', handleDelete);
+    zoomPicture.addEventListener('click', handleZoom);
+
+    return placeItem;
+} //======================== preparing Place Item - END
+
+//======================== show Place Item with data
 const renderPlaceItem = (placeItem) => {
   places.prepend(placeItem);
 }
 
-
-
-
-// open popup window
-
+//======================== open popup window
 function openPopup (popup){
   popup.classList.add('popup_opened');
 }
 
+//======================== close popup window
 function closePopup (popup){
   popup.classList.remove('popup_opened');
 }
+
+//======================== Submit handles
 
 const handleSubmitProfile = (e) => {
   e.preventDefault();
@@ -130,16 +104,13 @@ const handleSubmitPlace = (e) => {
   renderPlaceItem(createPlaceItem (placeData));
   closePopup(popupAddPlace);
 }
-//===================================================
-// const handleZoomFoto = (e) => {
-//   console.log(e.target);
-// }
 
-//====================================================
-// start page with some initial place Items
+//======================== Load page with some initial place Items
 initialCards.forEach((card) => {
   renderPlaceItem(createPlaceItem(card));
 });
+
+//======================== Adding Listeners on Bttn
 
 profileEditBttn.addEventListener('click',() => {
   popupInputName.value = profileName.textContent;
@@ -148,6 +119,8 @@ profileEditBttn.addEventListener('click',() => {
 });
 
 placeAddBttn.addEventListener('click',() => {
+  popupInputCaption.value = '';
+  popupInputLink.value = '';
   openPopup (popupAddPlace);
 });
 
@@ -155,8 +128,6 @@ popupCloseBttns.forEach( (bttn) => {
   const popup = bttn.closest('.popup');
   bttn.addEventListener ('click', () => closePopup (popup));
 });
-console.log(profileForm);
 
 profileForm.addEventListener('submit', handleSubmitProfile);
 addPlaceForm.addEventListener('submit', handleSubmitPlace);
-// elementFoto.addEventListener('click', handleZoomFoto);
